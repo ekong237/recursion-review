b3d43d6257a4418cf437f6d94730d30a4716dbae
Hack Reactor Student <student@hackreactor.com>
2017-08-21 12:04:03 -0700

    First draft

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,31 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(input) {
+  // determine if string
+    // if yes, add "" around string
+    // return string
+  // if input is array, loop through elements via stringifyJSON
+    // concat each element
+    // return string with brackets as a string
+  // if input is object
+    // for every element in object, stringify key and value
+    // return "{stringified key value}"
+  if (typeof(input) === 'string'){
+    return '"' + input + '"';
+  } else if (Array.isArray(input)){
+    var innerS = input.reduce(function(resultString, el, i){
+      var intoString = stringifyJSON(el);
+      return resultString + intoString + ',';
+    },'');
+    return '[' + innerS + ']';
+  } else if (typof(input) === 'object'){
+     var keys = Object.keys(input); 
+     var innerO = keys.reduce(function(resultString, el, i){
+       var objectIntoString = stringifyJSON(el);
+       var objectValIntoString = stringifyJSON(input[i]);
+       return resultString + objectIntoString + ':' + objectValIntoString + ',';
+    },'');
+      return '{' + innerO + '}';
+  }
 };
